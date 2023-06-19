31725361a139968b528ce2a20e3449de14d4380b
Jonathan Ivany <117655972+JonnyNL@users.noreply.github.com>
2023-06-19 11:43:20 -0230

    Created CLI function housings
    Empty CLI functions for splitting up between members

---
+++ cliFunction.js
@@ -0,0 +1,64 @@
+// All functions to make the CLI menu work
+
+// Function to create directory structure
+function createDirectoryStructure() {
+  // Enter code here...
+}
+
+// Function to create config file
+function createConfigFile() {
+  // Enter code here...
+}
+
+// Function to create help filesc
+function createHelpFiles() {
+  // Enter code here...
+}
+
+// Function to view current config settings
+function viewConfigSettings() {
+  // Enter code here...
+}
+
+// Function to reset config file to default settings
+function resetConfigFile() {
+  // Enter code here...
+}
+
+// Function to update specific config setting
+function setConfigSetting(option, value) {
+  // Enter code here...
+}
+
+// Function that counts tokens
+function countTokens() {
+  // Enter code here...
+}
+
+// Function that generates a token for a given username
+function generateToken(username) {
+  // Enter code here...
+}
+
+// Function that updates token entry (phone or email) for a given username
+function updateToken(tokenType, username, value) {
+  // Enter code here...
+}
+
+// Function that searches for token based on username, email, or phone
+function searchToken(searchType, value) {
+  // Enter Code here...
+}
+
+module.exports = {
+  createDirectoryStructure,
+  createConfigFile,
+  createHelpFiles,
+  viewConfigSettings,
+  resetConfigFile,
+  setConfigSetting,
+  countTokens,
+  generateToken,
+  updateToken,
+  searchToken,
+};
