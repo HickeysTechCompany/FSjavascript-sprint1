105dd678a7822a9429bbcb21ad46572175e1a958
Jonathan Ivany <117655972+JonnyNL@users.noreply.github.com>
2023-06-26 18:11:40 -0230

    createHelpFiles
    Function for cli command node cli init --cat now fully working. Logs to be added
    
    Co-Authored-By: TylerN132 <117655789+TylerN132@users.noreply.github.com>

--- cliFunctions/init.js
+++ cliFunctions/init.js
@@ -1,31 +1,29 @@
 // Function to create config file
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 
-function createConfigFile() {
+async function createConfigFile() {
   const { configjson } = require("../templates.js"); // Import only configjson
 
   const configDir = path.join(__dirname, "..", "json"); // Create json directory at one level up
   const configFilePath = path.join(configDir, "config.json");
 
   // Ensure that the 'json' directory exists
-  if (!fs.existsSync(configDir)) {
-    fs.mkdirSync(configDir);
+  try {
+    await fs.access(configDir);
+  } catch (err) {
+    await fs.mkdir(configDir);
   }
 
   // Check if the config file already exists
-  fs.access(configFilePath, fs.constants.F_OK, (err) => {
-    if (!err) {
-      console.log("Config file already exists.");
-      return;
-    }
-
+  try {
+    await fs.access(configFilePath);
+    console.log("Config file already exists.");
+  } catch (err) {
     // Write the configjson data to the config file
-    fs.writeFile(configFilePath, JSON.stringify(configjson, null, 2), (err) => {
-      if (err) throw err;
-      console.log("Created a new configuration file.");
-    });
-  });
+    await fs.writeFile(configFilePath, JSON.stringify(configjson, null, 2));
+    console.log("Created a new configuration file.");
+  }
 }
 
 // Function to create directory structure
@@ -33,9 +31,52 @@ function createDirectoryStructure() {
   // Enter code here...
 }
 
-// Function to create help filesc
-function createHelpFiles() {
-  // Enter code here...
+// Function to create help files
+const {
+  allhelptxt,
+  confighelptxt,
+  tokenhelptxt,
+  inithelptxt,
+} = require("../templates.js");
+
+async function createHelpFiles() {
+  try {
+    const helpDir = path.join(__dirname, "..", "commands");
+    const helpFiles = [
+      { name: "inithelp.txt", content: inithelptxt },
+      { name: "confighelp.txt", content: confighelptxt },
+      { name: "tokenhelp.txt", content: tokenhelptxt },
+      { name: "allhelp.txt", content: allhelptxt },
+    ];
+
+    // Check if the "commands" help directory exists
+    const isDirExists = await fs
+      .stat(helpDir)
+      .then((stats) => stats.isDirectory());
+
+    if (!isDirExists) {
+      // Create the "commands" directory if it doesn't exist
+      await fs.mkdir(helpDir);
+      console.log(`Directory ${helpDir} created for help files`);
+    } else {
+      console.log(`Directory ${helpDir} found for help files`);
+    }
+
+    // Check and create the help files
+    for (const helpFile of helpFiles) {
+      const filePath = `${helpDir}/${helpFile.name}`;
+
+      try {
+        await fs.stat(filePath);
+        console.log(`${helpFile.name} already exists`);
+      } catch (error) {
+        await fs.writeFile(filePath, helpFile.content);
+        console.log(`${helpFile.name} was created`);
+      }
+    }
+  } catch (error) {
+    console.error("Error creating help files:", error);
+  }
 }
 
 module.exports = {
