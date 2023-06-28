cae502461729bc4e7da597eaedbb1204e63df390
Jon Ivany <117655972+JonnyNL@users.noreply.github.com>
2023-06-28 21:02:04 -0230

    Updated comments for clarity

--- cli.js
+++ cli.js
@@ -1,26 +1,36 @@
+// Importing necessary filesystem and path modules from Node.js
 const fs = require("fs");
 const path = require("path");
+
+// Importing custom handlers from 'handlers' directory
 const initHandler = require("./handlers/handleInit.js");
 const configHandler = require("./handlers/handleConfig.js");
 const tokenHandler = require("./handlers/handleToken.js");
 
-// Parse command-line arguments
+// Parsing command-line arguments
 const [nodePath, scriptPath, subcommand, ...options] = process.argv;
 
-// Get the filename without the extension to use as the command
+// Getting the filename without the extension to use as the command
 const filename = path.basename(scriptPath, path.extname(scriptPath));
 
+// Checking if a subcommand was provided
 if (subcommand === undefined) {
-  displayHelp("./commands.txt"); // Display commands.txt if subcommand is undefined
+  // If subcommand is undefined, displaying help from 'commands.txt'
+  displayHelp("./commands.txt");
 } else if (subcommand === "--help") {
+  // If '--help' is requested, displaying help from 'allhelp.txt'
   displayHelp("./commands/allhelp.txt");
 } else {
+  // Handling main command if a valid subcommand is given
   handleMainCommand(subcommand, options);
 }
 
+// Function to handle main commands
 function handleMainCommand(subcommand, options) {
   switch (subcommand) {
     case "init":
+      // If 'init' subcommand is given without options or with '--help', displaying help from 'inithelp.txt'
+      // Else, calling initHandler with provided options
       if (options.length === 0 || options.includes("--help")) {
         displayHelp("./commands/inithelp.txt");
       } else {
@@ -28,6 +38,8 @@ function handleMainCommand(subcommand, options) {
       }
       break;
     case "config":
+      // If 'config' subcommand is given without options or with '--help', displaying help from 'confighelp.txt'
+      // Else, calling configHandler with provided options
       if (options.length === 0 || options.includes("--help")) {
         displayHelp("./commands/confighelp.txt");
       } else {
@@ -35,6 +47,8 @@ function handleMainCommand(subcommand, options) {
       }
       break;
     case "token":
+      // If 'token' subcommand is given without options or with '--help', displaying help from 'tokenhelp.txt'
+      // Else, calling tokenHandler with provided options
       if (options.length === 0 || options.includes("--help")) {
         displayHelp("./commands/tokenhelp.txt");
       } else {
@@ -42,17 +56,22 @@ function handleMainCommand(subcommand, options) {
       }
       break;
     default:
+      // If an invalid subcommand is given, logging an error message
       console.log(`Invalid subcommand for ${filename}!`);
       break;
   }
 }
 
+// Function to display help messages
 function displayHelp(filePath) {
+  // Reading the help file using filesystem's readFile method
   fs.readFile(filePath, "utf8", (err, data) => {
+    // If an error occurs during reading, logging the error
     if (err) {
       console.error("Error reading file:", err);
       return;
     }
+    // Logging the contents of the help file
     console.log(data);
   });
 }
